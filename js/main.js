56a6bfa18810986ed88dfb073367ed73c067ffe2
linlyn15s <garridoailyn2017@gmail.com>
2026-04-12 22:29:11 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,22 +1,20 @@
 const botones = document.querySelectorAll('.btn-accion');
 
 botones.forEach(boton => {
-
-    // Guardamos el texto original UNA vez
     const textoOriginal = boton.textContent;
 
     boton.addEventListener('click', () => {
-
         const idObjetivo = boton.dataset.target;
         const caja = document.getElementById(idObjetivo);
 
-        if (caja.style.display === 'block') {
-            caja.style.display = 'none';
-            boton.textContent = textoOriginal; // vuelve al original
-        } else {
-            caja.style.display = 'block';
-            boton.textContent = '❌ Ocultar información';
-        }
+        // Cambiamos el toggle de 'active'
+        caja.classList.toggle('active');
 
+        // Lógica de cambio de texto
+        if (caja.classList.contains('active')) {
+            boton.textContent = 'Cerrar panel';
+        } else {
+            boton.textContent = textoOriginal;
+        }
     });
 });
